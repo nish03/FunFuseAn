157e60c74be6e6679c036dbda4cd606d1817ac44
nish03 <31392545+nish03@users.noreply.github.com>
2019-08-14 18:29:03 +0200

    Create test_preprocess.py

---
+++ FunFuseAn/test_preprocess.py
@@ -0,0 +1,40 @@
+##############This script preprocesses the testing data###############
+
+#import the relevant packages
+import os
+import natsort
+import glob
+import numpy as np
+import imageio
+from FunFuseAn.init_param import init_param
+
+def test_preprocess_mri():
+    filenames = os.listdir('/home/nora/Desktop/Datasets/Grayscale_modalities/Testing/MR-T2/')
+    dataset = os.path.join(os.getcwd(), '/home/nora/Desktop/Datasets/Grayscale_modalities/Testing/MR-T2/')
+    data = []
+    for ext in ('*.gif', '*.png', '*.jpg','*.tif'):
+        data.extend(glob.glob(os.path.join(dataset, ext)))
+    data.sort(key=lambda x:int(filter(str.isdigit, x)))
+    test_mri = np.zeros((len(data), image_width,image_length))
+    for i in xrange(len(data)):
+        test_mri[i,:,:] =(imageio.imread(data[i]))
+        test_mri[i,:,:] =(test_mri[i,:,:] - np.min(test_mri[i,:,:])) / (np.max(test_mri[i,:,:]) - np.min(test_mri[i,:,:]))
+        test_mri[i,:,:] = np.float32(test_mri[i,:,:])
+    
+    test_mri = test_mri[:,:,:,np.newaxis]
+
+
+def test_preprocess_pet():
+    filenames = os.listdir('/home/nora/Desktop/Datasets/Grayscale_modalities/Testing/CT/')
+    dataset = os.path.join(os.getcwd(), '/home/nora/Desktop/Datasets/Grayscale_modalities/Testing/CT/')
+    data = []
+    for ext in ('*.gif', '*.png', '*.jpg', '*.tif'):
+        data.extend(glob.glob(os.path.join(dataset, ext)))
+    data.sort(key=lambda var:[int(x) if x.isdigit() else x for x in re.findall(r'[^0-9]|[0-9]+', var)])
+    test_oth = np.zeros((len(data),image_width,image_length))
+    for i in xrange(len(data)):
+        test_pet[i,:,:] =(imageio.imread(data[i]))
+        test_pet[i,:,:] =(test_pet[i,:,:] - np.min(test_pet[i,:,:])) / (np.max(test_pet[i,:,:]) - np.min(test_pet[i,:,:]))
+        test_pet[i,:,:] = np.float32(test_oth[i,:,:])
+    
+    test_pet = test_pet[:,:,:,np.newaxis]
