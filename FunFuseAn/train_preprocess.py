a85af9d732bc543a2212e7e2639147afb1677078
nish03 <31392545+nish03@users.noreply.github.com>
2019-08-14 14:40:32 +0200

    Create train_preprocess.py

---
+++ FunFuseAn/train_preprocess.py
@@ -0,0 +1,39 @@
+#############This script preprocesses the training data ##############
+
+#import the relevant packages
+import os
+import natsort
+import glob
+import numpy as np
+import imageio
+from FunFuseAn import init_param
+
+def train_preprocess_mri():
+    filenames = os.listdir('/home/...../Training/MRI/')
+    dataset = os.path.join(os.getcwd(), '/home/...../Training/MRI/')
+    data = glob.glob(os.path.join(dataset, "*.gif"))
+    data = natsort.natsorted(data,reverse=False)
+    train_mri = np.zeros((len(data), image_width,image_length),dtype=float)
+    for i in range(len(data)):
+        train_mri[i,:,:] =(imageio.imread(data[i]))
+        train_mri[i,:,:] =(train_mri[i,:,:] - np.min(train_mri[i,:,:])) / (np.max(train_mri[i,:,:]) - np.min(train_mri[i,:,:]))
+        train_mri[i,:,:] = np.float32(train_mri[i,:,:])
+    
+    train_mri = train_mri[:,:,:,np.newaxis]   
+    
+ 
+def train_preprocess_pet():    
+    filenames = os.listdir('/home/...../Training/PET/')
+    dataset = os.path.join(os.getcwd(), '/home/...../Training/PET/')
+    data = glob.glob(os.path.join(dataset, "*.gif"))
+    data = natsort.natsorted(data,reverse=False)
+    train_pet = np.zeros((len(data),image_width,image_length),dtype=float)
+    for i in range(len(data)):
+        train_pet[i,:,:] =(imageio.imread(data[i]))
+        train_pet[i,:,:] =(train_pet[i,:,:] - np.min(train_pet[i,:,:])) / (np.max(train_pet[i,:,:]) - np.min(train_pet[i,:,:]))
+        train_pet[i,:,:] = np.float32(train_pet[i,:,:])
+        
+    train_pet = train_pet[:,:,:,np.newaxis]
+    
+    
+  
