053e173f249f81d8cc25c3744dea1dd5fe23088a
nish03 <31392545+nish03@users.noreply.github.com>
2019-08-14 14:40:28 +0200

    Update init_param.py

--- FunFuseAn/init_param.py
+++ FunFuseAn/init_param.py
@@ -1,17 +1,18 @@
 ############define the hyperparameters#############
 
-#define the dimensions of the image
-image_length = 256
-image_width  = 256
+def init_param():
+    #define the dimensions of the image
+    image_length = 256
+    image_width  = 256
 
-#define the number of channels for input data
-gray_channels = 1
+    #define the number of channels for input data
+    gray_channels = 1
 
-#define the batch size during training
-batch_size   = 1
+    #define the batch size during training
+    batch_size   = 1
 
-#define the number of epochs 
-epoch = 200
+    #define the number of epochs 
+    epoch = 200
 
-#define the learning rate
-lr = 0.002
+    #define the learning rate
+    lr = 0.002
