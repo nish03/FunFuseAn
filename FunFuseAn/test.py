c32dbb0e521194f982dc72351c955f3b16f16e56
nish03 <31392545+nish03@users.noreply.github.com>
2019-08-14 23:56:00 +0200

    Update test.py

--- FunFuseAn/test.py
+++ FunFuseAn/test.py
@@ -4,14 +4,28 @@
 import tensorflow as tf
 from FunFuseAn.init_param import init_param
 from FunFuseAn.network import network
+from FunFuseAn.test_preprocess import test_preprocess_mri, test_preprocess_pet
 
-
+#define placeholders
 image_length, image_width, gray_channels, batch_size, epoch, lr, pet_image, mri_image = init_param()
 
+#define the network
+fused_image = network(mri_image,pet_image)
+
+#import test images
+test_mri = test_preprocess_mri()
+test_pet = test_preprocess_pet()
+
 #run the test session
+fused_test_image = []
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
     saver.restore(sess, '/home/....../Checkpoint_lambda_0/')
-    img = sess.run(fused_image, feed_dict={images_mri: test_mri, 
-                                           images_pet: test_pet}
-                   )
+    for idx in range(0, batch_idxs):
+      batch_test_mri = test_mri[idx*batch_size : (idx+1)*batch_size,:,:,:]
+      batch_test_pet = test_pet[idx*batch_size : (idx+1)*batch_size,:,:,:]
+      img = sess.run(fused_image, feed_dict={images_mri: test_mri, 
+                                             images_pet: test_pet})
+      fused_test_image.append(img) 
+    
+
