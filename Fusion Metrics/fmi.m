db53be61c68e7dd3d930c6a1bc463e49dbbff4bb
Nishant Kumar <31392545+nish03@users.noreply.github.com>
2020-08-28 16:50:17 +0200

    Updated author info and citation details

--- Fusion Metrics/fmi.m
+++ Fusion Metrics/fmi.m
@@ -1,3 +1,12 @@
+#Feature Mutual Information (FMI) - non-reference image fusion metric
+
+#FMI calculates the Feature Mutual Information (FMI), the non-reference performance metric for image fusion algorithms, proposed in:
+#M.B.A. Haghighat, A. Aghagolzadeh, H. Seyedarabi, "A Non-Reference Image Fusion Metric Based on Mutual Information of Image Features," Computers and Electrical Engineering, vol. 37, no. 5, pp. 744-756, Sept. 2011. http://dx.doi.org/10.1016/j.compeleceng.2011.07.012
+#This code is the implementation of FAST-FMI, presented in:
+#M. Haghighat, M.A. Razian, “Fast-FMI: non-reference image fusion metric,” 8th International Conference on Application of Information and Communication Technologies (AICT), pp. 1-3, 2014. http://dx.doi.org/10.1109/ICAICT.2014.7036000
+#(C) Mohammad Haghighat, University of Miami haghighat@ieee.org PLEASE CITE THE ABOVE PAPERS IF YOU USE THIS CODE.
+
+
 function nfmi = fmi(ima, imb, imf, feature, w)
 
 if nargin < 3               % Check correct number of arguments
